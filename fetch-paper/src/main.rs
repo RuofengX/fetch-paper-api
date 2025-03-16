41425f8da9a90476ea0ef8d253c8cf92e3ea476d
ruofeng <i@ruofengx.cn>
2025-03-16 17:59:21 +0800

    add log

--- fetch-paper/src/main.rs
+++ fetch-paper/src/main.rs
@@ -3,6 +3,7 @@ use std::path::PathBuf;
 use anyhow::{anyhow, Result};
 use clap::{ArgAction, Parser};
 use fetch_paper_lib::Root;
+use log::{error, info, warn};
 
 #[derive(Parser)]
 #[command(about)]
@@ -28,6 +29,8 @@ struct Cli {
 
 #[tokio::main]
 async fn main() -> Result<()> {
+    colog::init();
+
     let cli = Cli::parse();
 
     if cli.path.exists() {
@@ -36,9 +39,9 @@ async fn main() -> Result<()> {
 
     let root = Root::new().await?;
     if !root.projects.contains(&cli.project) {
-        println!("available projects:");
+        info!("available projects:");
         for p in root.projects {
-            println!("\t{}", p)
+            info!("\t{}", p)
         }
         return Err(anyhow!(format!("project <{}> not found", cli.project)));
     }
@@ -48,7 +51,7 @@ async fn main() -> Result<()> {
         if project.versions.contains(&target_version) {
             project.get_version(&target_version).await?
         } else {
-            println!("available versions: {:?}", project.versions);
+            info!("available versions: {:?}", project.versions);
             return Err(anyhow!(format!("version <{target_version}> not found")));
         }
     } else {
@@ -59,27 +62,25 @@ async fn main() -> Result<()> {
         if version.builds.contains(&target_build) {
             version.get_build(target_build).await?
         } else {
-            println!("available builds: {:?}", version.builds);
+            info!("available builds: {:?}", version.builds);
             return Err(anyhow!(format!("build <{target_build}> not found")));
         }
     } else {
         version.get_latest_build().await?
     };
 
-    println!("start download >> {}", build.download_link());
-    println!("download path >> {}", cli.path.to_string_lossy());
-    println!("remote sha256 >> {}", build.download_digest_sha256());
+    warn!("start download >> {}", build.download_link());
+    warn!("download path >> {}", cli.path.to_string_lossy());
+    warn!("remote sha256 >> {}", build.download_digest_sha256());
     build.download(&cli.path).await?;
-    println!("download done");
+    info!("download done");
 
     if !cli.skip_checksum {
-        print!("checking sha256 >> ");
+        info!("checking sha256 >> ");
         if build.checksum(&cli.path).await? {
-            print!("pass");
-            println!();
+            info!("pass");
         } else {
-            print!("fail");
-            println!();
+            error!("fail");
             return Err(anyhow!("sha256 check fail"));
         }
     }
